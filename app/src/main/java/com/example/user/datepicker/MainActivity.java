0973e3894405e04a1181ed6e9a5014883b172fef
gitfiroz <firoziceiu@gmail.com>
2018-04-18 20:27:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/user/datepicker/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.user.datepicker;
+
+import android.app.DatePickerDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button showDateButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showDateButton =findViewById(R.id.showDateButtonID);
+        showDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        Toast.makeText(MainActivity.this, dayOfMonth+"/"+(month+1)+"/"+year, Toast.LENGTH_SHORT).show();
+                    }
+                }, 2018, 3, 10);
+               // Toast.makeText(MainActivity.this, "Date Picker", Toast.LENGTH_SHORT).show();
+                datePickerDialog.show();
+            }
+
+        });
+    }
+}
